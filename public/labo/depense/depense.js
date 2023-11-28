78055d469b8a74f50e836de259352f64f7c28754
Verronne <muzardjulien1986@gmail.com>
2023-11-28 10:21:26 +0100

    mon premier commit
    version fonctionnel

--- public/labo/depense/depense.js
+++ public/labo/depense/depense.js
@@ -12,6 +12,7 @@ let grandTotal = document.getElementById("grandTotal")
 let boutonOKgauche = document.getElementById("boutonOKgauche")
 let boutonOKdroite = document.getElementById("boutonOKdroite")
 let totalGainlist = document.getElementById("totalGain")
+let totalDepense = document.getElementById("totalDepense")
 
 //variable global gain
 let totalGain=0
@@ -22,13 +23,27 @@ let grandTotalNum=0
 
 //attribution de la saisi du nom et du prix
 
+//////////////////////// page gauche //////////////////////////
+
 boutonOKgauche.addEventListener("click", () => {
 
     //réccupération des champs saisi par l'utilisateur
     let _gainText = gainText.value
     let _gainMontant = gainMontant.value
 
+    if(gainText.value === "" || gainMontant.value === "") {
+        alert("vous devez entrez les deux champs.");
+        return;
+    }
+    else {
+    
+
+    grandTotalNum+= parseFloat(_gainMontant)
+
+    grandTotal.textContent = grandTotalNum.toString()
     //total gain 
+    //parseFloat permet de convertir une chaine de caractère en nombre à virgule
+    //voir aussi parseInt (pour les entiers)
     totalGain +=  parseFloat(_gainMontant)
     totalGainlist.textContent= totalGain.toString()
 
@@ -63,14 +78,102 @@ boutonOKgauche.addEventListener("click", () => {
     //la ligne total
     gainListing.appendChild.ligneDeGain
 
+    gainText.value=""
+    gainMontant.value=""
+
 boutonSuppresion.addEventListener("click", ()=> {
-    let warning=document.createElement("div")
-    ligneDeGain.remove()
+
+    let récupererValeurDeLaDiv = boutonSuppresion.parentElement.children[1].innerHTML;
+//soustraction de totalGain
+
+totalGain =  totalGain - parseFloat(récupererValeurDeLaDiv);
+grandTotalNum -= parseFloat(récupererValeurDeLaDiv);
+
+grandTotal.textContent = grandTotalNum
+//totalGain -= ....
+
+//changement de la valeur de totalGainList(élément Html) avec la valeur de totalGain(numérique)
+totalGainlist.textContent = totalGain.toString()
+
+    boutonSuppresion.parentElement.remove()
     
 })
-
-
-
-
+    }
 })
 
+//############################ page droite #################################
+
+
+boutonOKdroite.addEventListener("click", () => {
+
+    //récupération des champs saisi par l'utilisateur
+    let _depenseText = depenseText.value
+    let _depenseMontant = depenseMontant.value
+
+    if(depenseText.value ==="" || depenseMontant.value ==="") {
+        alert("vous devez entrez les deux champs");
+        return;
+    }
+    else {
+
+    grandTotalNum-= parseFloat(_depenseMontant)
+
+    grandTotal.textContent = grandTotalNum.toString()
+    //total gain 
+    //parseFloat permet de convertir une chaine de caractère en nombre à virgule
+    //voir aussi parseInt (pour les entiers)
+     totalperte += parseFloat(_depenseMontant)
+     totalDepense.textContent= totalperte.toString()
+
+    // création div principal depense
+    let ligneDeDepense = document.createElement("div")
+    ligneDeDepense.classList.add("ligneDeGain")
+
+    //création des div secondaire (le libellé, le prix)
+    // création de la div pour le texte
+
+    let depenseTextDiv = document.createElement("div")
+    depenseTextDiv.textContent=_depenseText
+
+    //ajout de la div text à la div générale
+    ligneDeDepense.appendChild(depenseTextDiv)
+    
+    //ajout de la div générale à la LISTE DEPENSE
+    depenseListing.appendChild(ligneDeDepense)
+
+    //création du prix sur la div listing
+    let depenseMontantDiv = document.createElement("div")
+    depenseMontantDiv.textContent = _depenseMontant
+    //création du bouton suppression
+    let boutonSuppresion = document.createElement("img")
+    boutonSuppresion.src = "./poubelle.png"
+    boutonSuppresion.classList.add("poubelle")
+    //le prix au milieu
+    ligneDeDepense.appendChild(depenseMontantDiv)
+    //le bouton suppression
+    ligneDeDepense.appendChild(boutonSuppresion)
+    //la ligne depense total
+    depenseListing.appendChild.ligneDeDepense
+
+    //remise a zéro des entrer
+    depenseText.value = ""
+    depenseMontant.value = ""
+
+    boutonSuppresion.addEventListener("click",()=> {
+        let récupererValeurDeLaDiv = boutonSuppresion.parentElement.children[1].innerHTML;
+
+        //soustraction de totaldepense
+totalperte -= parseFloat(récupererValeurDeLaDiv)
+
+
+        //changement de la valeur de totalDepenseList(élément Html) avec la valeur totalDepense(numérique)
+        
+        console.log(grandTotalNum)
+        grandTotalNum += parseFloat(récupererValeurDeLaDiv);
+        grandTotal.textContent = grandTotalNum
+        totalDepense.textContent = totalperte.toString()
+
+        boutonSuppresion.parentElement.remove()
+    })
+}
+})
--- public/labo/labojavascript/script.js
+++ public/labo/labojavascript/script.js
@@ -1,45 +1,87 @@
-const bouton = document.getElementById("bouton")
+let tableauMois=["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"]
+
+//################ BOUCLE SIMPLE ##################
+
+//déclaration d'un tableau de chaine de caractere contenant par exemple les mois de l'année 
+/*let tableauMois =["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"];
+//récupération de la div affichage
+let affichage = document.getElementById("affichage");
+/boucle simple d'itération
+//tableauMois.lenght = 12
+for(let index = 0;index<tableauMois.length;index++){
+    affichage.textcontent = tableauMois[index];
+    //à chaque itération, modification du texte de la div affichage
+    if(tableauMois[index]==="mai"|| tableauMois[index]=== "septembre") {
+        console.log("le mois de "+ tableauMois[index] +" à été trouvé",index)
+    }
+}
+console.log(tableauMois);*/
+
+
+//#################### BOUCLE SIMPLE A INCREMENTATION ######################
+
+//déclaration d'un tableau de chaine de caractere contenant par exemple les mois de l'année 
+/*let tableauMois =["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"];
+//récupération de la div "affichage"
 
 let affichage = document.getElementById("affichage")
+//boucle simple d'iteration
+//tableauMois.length = 12
+for(let index =0;index<tableauMois.length;index+=2){
+    //à chaque itération l'index est incrémenté de 2, lit la valeur de tableauMois[index] et modifie le texte de la div affichage
+    affichage.textContent = tableauMois[index]
+    if(tableauMois[index]==="mai" || tableauMois[index]==="septembre"){
+        console.log("le mois de "+ tableauMois[index] + " à été trouvé", index)
+    }
+}*/
 
-let index=0;
+//############## BOUCLE SIMPLE AVEC 'CONTINUE' ###################
 
-let carreProduit = true
-bouton.addEventListener("click", ()=> {
+/*let tableauMois =["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"];
+let affichage = document.getElementById("affichage")
+for(let index = 0;index<tableauMois.length; index++) {
+    //si la valeur de la cellule vaut "juin", juin, continuer sans faire de traitement, sinon afficher la valeur de la cellule
+    if(tableauMois[index]==="juin"){
+        continue;
+    }
+    affichage.textContent = tableauMois[index];
+}
+console.log(tableauMois)*/
 
- 
+//############# BOUCLE SIMPLE AVEC L4INSTRUCTION 'BREAK'#######################
 
-    index++;
-    console.log("le bouton a été clické")
-    //déclanchement de processus 
-if(carreProduit) {
-    //déclaration d'une variable pour stocker un nouvelle élément html
-    let carre = document.createElement("div");
-    //ajout de la class carre
-    carre.classList.add("carre");
-    //ajout de l'élément au body du document
-    document.body.appendChild(carre);
-    if(index > 3){
-    carreProduit=false;
-    window.alert("vous avez cliqué"+index+"fois");
-
-  
+/*let tableauMois=["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"]
+let affichage = document.getElementById("affichage")
+for(let index = 0;index<tableauMois.length; index++){
+    if(tableauMois[index] === "mai"){
+    break;}
+    console.log(index)
+    affichage.textContent = tableauMois[index];
     
+}*/
+
+//#################### BOUCLE WHILE ##################
+
+let affichage = document.getElementById("affichage")
+//boucle d'iteration
+//tableauMois.length = 12
+//récupération de tous les élément possedant la classe "moisClass"
+//déclaration d'un index à incrémenter
+
+let index = 0;
+//passage dans le tableauMois à la recherche des éléments possedant la class "violet"
+//tant que l'élément possède la classe violet et que son texte est différent de novembre.
+while(tableauMois[index].classList.contains(".violet")&& tableauMois[index].innerHTML !=="novembre"){
+    //ajout de la class red qui colore la bordure en rouge
+    tableauMois[index].add("red")
+    //ajout d'une class pour un fond jaune
+    tableauMois[index].classList.add("addFondJaune")
+    //récupération du text de la div, stockage dans la variable textEnMajuscule et conversion en capitale
+    let textEnMajuscule = tableauMois[index].innerHTML.toUpperCase();
+    //assignation de la variable textEnMajuscule à la propriété text (innerhtml) de l'élément
+    tableauMois[index].innerHTML = textEnMajuscule
+    index++;
 }
-}
-let saisieUtilisateur = document.getElementById("entree_utilisateur")
-console.log(saisieUtilisateur.value)
 
-
-
-    //modification du texte de l'élément id = affichage
-    affichage.textContent = "hello!!!"
-    let tousLesCercles = document.querySelectorAll(".cercle")
-//boucle let i
-for(let i=0;i<tousLesCercles.length;i++){
-    tousLesCercles[i].style.backgroundColor = "red"
-
-}
-})
 
 
